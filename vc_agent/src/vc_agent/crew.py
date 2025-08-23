4ae0763fe3c3a084f7daa94a3b464c2c564d0643
Samiya Sehgal <samiyasehgal@gmail.com>
2025-08-23 22:10:45 +0530

    Update crew.py

--- vc_agent/src/vc_agent/crew.py
+++ vc_agent/src/vc_agent/crew.py
@@ -60,7 +60,8 @@ class VcAgent():
         return Crew(
             agents=self.agents, # Automatically created by the @agent decorator
             tasks=self.tasks, # Automatically created by the @task decorator
-            process=Process.sequential,
+            
+            process=Process.sequential,   # sequential can eb changes there are other options also 
             verbose=True,
             # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
